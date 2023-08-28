a665bbb2313911e6a08926c918de307c3bab800f
Wendell Vieira <wendell.vieiracunha@gmail.com>
2023-08-28 13:57:42 -0300

    feat: add entity of rainbow

--- backend/src/entities/rainbow.entity.ts
+++ backend/src/entities/rainbow.entity.ts
@@ -1,1 +1,22 @@
-export class Rainbow {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Rainbow {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	fullName: string;
+
+	@Column()
+	document: string;
+
+	@Column()
+	email: string;
+
+	@Column()
+	color: string;
+
+	@Column()
+	comments: string;
+}
